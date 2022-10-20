24abba5195f019681780ad6cb8eadafd1b5c57fb
Samuel Salazar <blancosamuel35@gmail.com>
2022-10-20 00:09:27 -0500

    Cuerpo

---
+++ main.js
@@ -0,0 +1,20 @@
+const quizdata = [
+  {
+    prugunta: "¿Cuántos decimales tiene el número pi π?",
+    a: "Dos",
+    b: "Cien",
+    c: "Infinito",
+    d: "Mil",
+    e: "Veinte",
+    correct: "c",
+  },
+  {
+    prugunta: "¿Cuánto tiempo tarda la luz del Sol en llegar a la Tierra?",
+    a: "12 min",
+    b: "1 día",
+    c: "12 horas",
+    d: "8 min",
+    e: "5 segundos",
+    correct: "d"
+  },
+];
